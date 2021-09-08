e827463f622843f0fb793a8a7a921b0bb29aeede
Merle Tyson Mills <tysonmills5@gmail.com>
2021-09-08 11:22:07 -0400

    changed styling

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const url = "https://swapi.dev/api/films/"
+//need to populate the section when clicked on 
+//then a way to show the user
 
-document.addEventListener("DOMContentLoaded", fetchInfo);
 
-function fetchInfo () {
-    fetch(url)
-        .then(response => response.json())
-        .then((json) => {
-      console.log(json)
-    })
-}
+
+//CONSTANTS-----------------------------------------
+const rootUrl = "https://swapi.dev/api/"
+const peopleUrl = "https://swapi.dev/api/people/"
+const filmsUrl = "https://swapi.dev/api/films/"
+const starshipsUrl = "https://swapi.dev/api/starships/"
+//const vehiclesUrl = "https://swapi.dev/api/vehicles/"
+//const speciesUrl = "https://swapi.dev/api/species/"
+const planetsUrl = "https://swapi.dev/api/planets/"
+
+// document.addEventListener("DOMContentLoaded", fetchInfo);
+
+// function fetchInfo () {
+//     fetch(planetsUrl)
+//         .then(response => response.json())
+//         .then((json) => {
+//       console.log(json)
+//     })
+// }
+// fetchInfo();
+
+//
